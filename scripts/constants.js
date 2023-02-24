aa1d217b43c55574202bb8dfe5fe9386a8a38020
Derek Woo <me@derekw.co>
2023-02-24 12:10:18 -0800

    Add constants.js to load navbar and footer

---
+++ scripts/constants.js
@@ -0,0 +1,6 @@
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./constants/navbar.html'));
+    console.log($('#footerPlaceholder').load('./constants/footer.html'));
+}
+
+loadSkeleton(); 
