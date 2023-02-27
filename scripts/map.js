a263c96364ba505d168f52af94fb705ab33a39b0
Derek Woo <me@derekw.co>
2023-02-26 21:45:43 -0800

    Add map component and scripts to map.html

---
+++ scripts/map.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([49.203, -122.934], 11);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+map.on('click', function(e) {
+    alert(e.latlng);
+});
