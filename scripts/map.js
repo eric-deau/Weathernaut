cf1557c95b06221e9338a2a55cb19f9674191999
Derek Woo <me@derekw.co>
2023-02-27 20:37:59 -0800

    Update map id to not conflict with navbar item id

--- scripts/map.js
+++ scripts/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([49.203, -122.934], 11);
+var map = L.map('map-embed').setView([49.203, -122.934], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
