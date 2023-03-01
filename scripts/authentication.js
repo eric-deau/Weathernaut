8adc57037ebb60a955da2ed2ca9ccd5ae26638a6
ERIC DEAU <edeau@my.bcit.ca>
2023-03-01 11:47:02 -0800

    Add authentication to new and existing users

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,25 +1,39 @@
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      var user = authResult.user;                            // get the user object from the Firebase authentication database
+      if (authResult.additionalUserInfo.isNewUser) {         //if new user
+        db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
+          name: user.displayName,                    //"users" collection
+          email: user.email,                         //with authenticated user's ID (user.uid)
+        }).then(function () {
+          console.log("New user added to firestore");
+          window.location.assign("main.html");       //re-direct to main.html after signup
+        }).catch(function (error) {
+          console.log("Error adding new user: " + error);
+        });
+      } else {
         return true;
-      },
-
-      uiShown: function() {
-        document.getElementById('loader').style.display = 'none';
       }
+      return false;
     },
 
-    signInFlow: 'popup',
-    signInSuccessUrl: './main.html',
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
+    uiShown: function () {
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
 
-    tosUrl: '<your-tos-url>',
+  signInFlow: 'popup',
+  signInSuccessUrl: './main.html',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
 
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
+  tosUrl: '<your-tos-url>',
 
-  ui.start('#firebaseui-auth-container', uiConfig);
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+ui.start('#firebaseui-auth-container', uiConfig);
