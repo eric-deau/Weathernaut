2dcd5cc5e9032b22ef6050259664fccc3932a5df
Derek Woo <me@derekw.co>
2023-03-01 19:33:53 -0800

    Add personalized greeting to main.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,36 @@
-function insertNameFromFirestore() {
-    // to check if the user is logged in:
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid); // let me to know who is the user that logged in to get the UID
-            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-            currentUser.get().then(userDoc => {
-                //get the user name
-                var userName = userDoc.data().name;
-                console.log(userName);
-                $("#name-goes-here").text(userName); //jquery
-            })
-        }
-    })
+function getTimeOfDay() {
+  var today = new Date();
+  var hourNow = today.getHours();
+  var greeting;
+
+  if (hourNow > 18) {
+    greeting = "Good evening";
+  } else if (hourNow > 12) {
+    greeting = "Good afternoon";
+  } else if (hourNow > 0) {
+    greeting = "Good morning";
+  } else {
+    greeting = "Welcome";
+  }
+
+  $("#greeting").text(greeting); //jquery
 }
-insertNameFromFirestore()
+
+function insertNameFromFirestore() {
+  // to check if the user is logged in:
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log(user.uid); // let me to know who is the user that logged in to get the UID
+      currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+      currentUser.get().then((userDoc) => {
+        //get the user name
+        var userName = userDoc.data().name;
+        console.log(userName);
+        $("#name-goes-here").text(userName); //jquery
+      });
+    }
+  });
+}
+
+getTimeOfDay();
+insertNameFromFirestore();
