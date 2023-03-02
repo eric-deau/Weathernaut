82ca25027bdfd88deb621f3faf2918da6a933a23
ERIC DEAU <edeau@my.bcit.ca>
2023-03-01 17:43:02 -0800

    Add mapinfo.html to repo

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 var map = L.map('map-embed').setView([49.203, -122.934], 11);
 var marker;
 var popup = L.popup();
-var numOfMarkers = 0;
+var markerOnMap = false;
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,15 +14,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function onMapClick(e) {
     // alert(e.latlng);
     coordinates = (e.latlng);
-    popup.setLatLng(coordinates);
-    popup.openOn(map);
-    if (numOfMarkers == 0) {
-        marker = L.marker(coordinates).addTo(map);
-        popup.setContent("You clicked the map at " + coordinates.toString());
-        numOfMarkers++;
+    //popup.setLatLng(coordinates);
+    //popup.openOn(map);
+    if (markerOnMap == false) {
+        marker = L.marker(coordinates, { alt: 'Info' }).addTo(map).bindPopup("test");
+        // popup.setContent("You clicked the map at " + coordinates.toString());
+        markerOnMap = true;
     } else {
         marker.removeFrom(map);
-        numOfMarkers--;
+        markerOnMap = false;
     }
     // marker = L.marker(coordinates).addTo(map);
 }
