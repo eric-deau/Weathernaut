a6a04bdd99ec3852d42122bc42dee9c94792457d
ERIC DEAU <edeau@my.bcit.ca>
2023-03-01 17:43:28 -0800

    Add lines for user authentication check

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,6 +2,17 @@ function loadSkeleton() {
     console.log($('#navbarPlaceholder').load('./constants/navbar.html'));
     console.log($('#footerPlaceholder').load('./constants/footer.html'));
     console.log($('#footer_loginPlaceholder').load('./constants/footer_login.html'));
+    // firebase.auth().onAuthStateChanged(function (user) {
+    //     if (user) {
+    //         // User is signed in.
+    //         // Do something for the user here.
+    //         console.log($('#navbarPlaceholder').load('../constants/navbar.html'));
+    //         console.log($('#footerPlaceholder').load('../constants/footer.html'));
+    //     } else {
+    //         // No user is signed in.
+    //         console.log($('#navbarPlaceholder').load('../constants/navbar.html'));
+    //         console.log($('#footerPlaceholder').load('../constants/footer.html'));
+    //     }
+    // });
 }
-
-loadSkeleton(); 
+loadSkeleton(); //invoke the function
