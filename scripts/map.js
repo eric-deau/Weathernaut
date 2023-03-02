abc7a5430f42869cda501cdab421ef581ff12cf0
Derek Woo <me@derekw.co>
2023-03-01 19:35:18 -0800

    Refactor map.js to use params for location & zoom

--- scripts/map.js
+++ scripts/map.js
@@ -1,30 +1,46 @@
-var map = L.map('map-embed').setView([49.203, -122.934], 11);
-var marker;
-var popup = L.popup();
-var markerOnMap = false;
+function createMap(longitude, latitude, zoom) {
+  longitude ? (longitude = longitude) : (longitude = 49.203);
+  latitude ? (latitude = latitude) : (latitude = -122.934);
+  zoom ? (zoom = zoom) : (zoom = 11);
 
+  var map = L.map("map-embed").setView([longitude, latitude], zoom);
+  var marker;
+  var popup = L.popup();
+  var markerOnMap = false;
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-
-// Event Clicker for Map
-function onMapClick(e) {
-    // alert(e.latlng);
-    coordinates = (e.latlng);
+  // Event Clicker for Map
+  function onMapClick(e) {
+    console.log(e.latlng);
+    coordinates = e.latlng;
     //popup.setLatLng(coordinates);
     //popup.openOn(map);
     if (markerOnMap == false) {
-        marker = L.marker(coordinates, { alt: 'Info' }).addTo(map).bindPopup("test");
-        // popup.setContent("You clicked the map at " + coordinates.toString());
-        markerOnMap = true;
+      marker = L.marker(coordinates, { alt: "Info" })
+        .addTo(map)
+        .bindPopup("test");
+      // popup.setContent("You clicked the map at " + coordinates.toString());
+      markerOnMap = true;
     } else {
-        marker.removeFrom(map);
-        markerOnMap = false;
+      marker.removeFrom(map);
+      markerOnMap = false;
     }
     // marker = L.marker(coordinates).addTo(map);
+  }
+
+  map.on("click", onMapClick);
 }
 
-map.on('click', onMapClick);
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+long = urlParams.get("long");
+lat = urlParams.get("lat");
+zoom = urlParams.get("zoom");
+
+createMap(long, lat, zoom);
