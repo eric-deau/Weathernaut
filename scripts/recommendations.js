078807070b8035ddb1ef1fbad20cb155b8b532da
Vishav-josan <vjosan1@my.bcit.ca>
2023-03-27 13:23:50 -0700

    classify weather cases for tips

--- scripts/recommendations.js
+++ scripts/recommendations.js
@@ -1,3 +1,47 @@
+// Define the API endpoint and your API key
+const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = "904aecba3bfef05b76019b6eb36cc2ea";
+
+// Get the user's current location using the Geolocation API
+navigator.geolocation.getCurrentPosition(async (position) => {
+  // Retrieve the latitude and longitude coordinates from the Geolocation API response
+  const { latitude, longitude } = position.coords;
+
+  // Construct the API request URL with the latitude, longitude, and API key
+  const apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  try {
+    // Fetch the current weather data from the OpenWeatherMap API
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // Extract the weather condition from the API response
+    const weatherCondition = data.weather[0].main.toLowerCase();
+
+    // Update the recommended page link and title based on the weather condition
+    updateRecommendationPage(getWeatherConditionCode(weatherCondition));
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+function getWeatherConditionCode(weatherCondition) {
+  switch (weatherCondition) {
+    case "rain":
+      return 1;
+    case "wind":
+      return 2;
+    case "clouds":
+    case "clear":
+      return 3;
+    case "snow":
+      return 4;
+    default:
+      return 0;
+  }
+}
+
+
 function updateRecommendationPage(weatherCondition) {
   // Get the recommended page link and title elements
   const recommendedPageLink = document.getElementById("recommendedPageLink");
@@ -27,17 +71,8 @@ function updateRecommendationPage(weatherCondition) {
       break;
     default:
       recommendedPageTitle.textContent = "Recommended Based on Current Weather: No Recommendations";
-      recommendedPageLink.href = "#";
+      recommendedPageLink.href = "funFacts.html";
       break;
   }
 }
 
-function removeCard(cardClassName) {
-  const currentWeatherCard = document.querySelector(`.row .${cardClassName}`);
-  if (currentWeatherCard) {
-    currentWeatherCard.parentNode.removeChild(currentWeatherCard);
-  }
-}
-
-
-updateRecommendationPage(4);
