55517b729759de02e7ffb9029c6fa01b89773478
Derek Woo <me@derekw.co>
2023-03-30 21:42:27 -0700

    Add back button to login.html

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,6 +2,7 @@ function loadSkeleton() {
     console.log($('#navbarPlaceholder').load('./constants/navbar.html'));
     console.log($('#footerPlaceholder').load('./constants/footer.html'));
     console.log($('#footer_loginPlaceholder').load('./constants/footer_login.html'));
+    console.log($('#navbar_loginPlaceholder').load('./constants/navbar_login.html'));
     // firebase.auth().onAuthStateChanged(function (user) {
     //     if (user) {
     //         // User is signed in.
