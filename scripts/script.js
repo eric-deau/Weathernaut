7948cd38faa69b3726e80d12e71a9215cad106e3
Derek Woo <me@derekw.co>
2023-04-01 19:00:39 -0700

    Remove redundant setup function in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,5 @@
-// function sayHello() {
-
-// }
-// //sayHello();
-
-// function goToLogin() {
-//   location.href = "login.html";
-// }
-
 function goToPage(e) {
   console.log($(this));
   dest = e.getAttribute("value");
   location.href = `${dest}.html`;
 }
-
-function setup() {
-  console.log("Setup");
-}
-
-setup();
